1c44888d92ef0958da526bc8aa80068712baaf2d
mildminihi <mildsupanat@hotmail.com>
2017-11-09 04:06:52 +0700

    decorate type button homeworklab09

--- lab/lab09/moneyflow/app/src/main/java/kmitl/lab09/supanat/moneyflow/AddActivity.java
+++ lab/lab09/moneyflow/app/src/main/java/kmitl/lab09/supanat/moneyflow/AddActivity.java
@@ -9,11 +9,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class AddActivity extends AppCompatActivity {
+public class AddActivity extends AppCompatActivity implements View.OnClickListener{
 
     private int typeCheck = 0;
-    public String type;
+    public String type = "";
 
 
 
@@ -22,6 +23,8 @@ public class AddActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Add Statement");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        Button btn_Save = (Button)findViewById(R.id.buttonSave);
+        btn_Save.setOnClickListener(this);
 
     }
 
@@ -31,8 +34,8 @@ public class AddActivity extends AppCompatActivity {
         Button btn_Outcome = (Button)findViewById(R.id.buttonExpence);
 
         if (typeCheck == 0 || typeCheck ==2) {
-            btn_Income.setBackgroundColor(Color.RED);
-            btn_Outcome.setBackgroundColor(Color.BLACK);
+            btn_Income.setBackgroundColor(Color.parseColor("#771C00"));
+            btn_Outcome.setBackgroundColor(Color.parseColor("#333333"));
             typeCheck = 1;
 
         }
@@ -48,8 +51,8 @@ public class AddActivity extends AppCompatActivity {
         Button btn_Income = (Button)findViewById(R.id.buttonIncome);
         Button btn_Outcome = (Button)findViewById(R.id.buttonExpence);
         if (typeCheck == 0 || typeCheck == 1) {
-            btn_Income.setBackgroundColor(Color.BLACK);
-            btn_Outcome.setBackgroundColor(Color.RED);
+            btn_Income.setBackgroundColor(Color.parseColor("#333333"));
+            btn_Outcome.setBackgroundColor(Color.parseColor("#771C00"));
             typeCheck = 2;
 
         }
@@ -60,7 +63,11 @@ public class AddActivity extends AppCompatActivity {
         }
 
     }
-    public void onSave(View view){
+
+
+
+    @Override
+    public void onClick(View view) {
         EditText state = (EditText)findViewById(R.id.editText);
         EditText money = (EditText)findViewById(R.id.editText2);
 
@@ -71,34 +78,39 @@ public class AddActivity extends AppCompatActivity {
             type = "-";
         }
 
-        final String list = state.getText().toString();
-        final int moneyResult = Integer.parseInt(money.getText().toString());
+
+
         final MessageDB messageDB = Room.databaseBuilder(getApplicationContext(),
                 MessageDB.class, "MONEYTABLE").build();
 
-        new AsyncTask<Void, Void, MoneyTableResult>(){
-
-            @Override
-            protected MoneyTableResult doInBackground(Void... voids) {
 
 
-                MoneyTableResult moneyTableResult = new MoneyTableResult();
-                moneyTableResult.setType(type);
-                moneyTableResult.setState(list);
-                moneyTableResult.setMoney(moneyResult);
+        if (state.getText().toString().equals("")|| money.getText().toString().equals("")|| type == ""){
+            Toast.makeText(AddActivity.this, "You Have Empty Field", Toast.LENGTH_LONG).show();
+        }
+        else{
+            final String list = state.getText().toString();
+            final int moneyResult = Integer.parseInt(money.getText().toString());
+            new AsyncTask<Void, Void, MoneyTableResult>(){
+
+                @Override
+                protected MoneyTableResult doInBackground(Void... voids) {
 
 
-                messageDB.getMessageInfoDAO().insert(moneyTableResult);
+                    MoneyTableResult moneyTableResult = new MoneyTableResult();
+                    moneyTableResult.setType(type);
+                    moneyTableResult.setState(list);
+                    moneyTableResult.setMoney(moneyResult);
 
-                return null;
-            }
-        }.execute();
-        Intent intent = new Intent();
-        setResult(RESULT_OK, intent);
-        finish();
+
+                    messageDB.getMessageInfoDAO().insert(moneyTableResult);
+
+                    return null;
+                }
+            }.execute();
+            Intent intent = new Intent();
+            setResult(RESULT_OK, intent);
+            finish();
+        }
     }
-
-
-
-
 }
